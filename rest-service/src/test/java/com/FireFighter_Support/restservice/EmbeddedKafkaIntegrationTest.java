53411f3c37dc263a8d9f81e5df06c07888e179d1
AlexkononovV <alexeykononov@ua.pt>
2021-06-20 22:36:10 +0100

    test com emb kfka
    --

--- rest-service/src/test/java/com/FireFighter_Support/restservice/EmbeddedKafkaIntegrationTest.java
+++ rest-service/src/test/java/com/FireFighter_Support/restservice/EmbeddedKafkaIntegrationTest.java
@@ -1,50 +1,72 @@
 package com.FireFighter_Support.restservice;
 
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-import com.FireFighter_Support.restservice.kafka.embedded.KafkaProducerTest;
-import com.FireFighter_Support.restservice.kafka.embedded.KafkaConsumerTest;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
-/*
+
+
+
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, topics = {"ESP33_SensorData"}, brokerProperties = { "listeners=PLAINTEXT://localhost:19092", "port=19092" })
 @SpringBootTest
-@DirtiesContext                                                             //192.168.160.18:19092
-@EmbeddedKafka(partitions = 1  , brokerProperties = { "listeners=PLAINTEXT://192.168.160.18:19092", "port=19092" })
-class EmbeddedKafkaIntegrationTest {
+public class EmbeddedKafkaIntegrationTest {
+
+    private static String TEST_TOPIC = "ESP33_SensorData";
+    private String testMsg = "{'firefighters': [{'CO': '36', 'temp': '23', 'hum': '38', 'bat': '0', 'lat': '40.06483992', 'longi': '-8.16039721', 'alt': '1132', 'hr': '101.525187124'}, {'CO': '0', 'temp': '23', 'hum': '39', 'bat': '93', 'lat': '40.06469093', 'longi': '-8.16050738', 'alt': '1139', 'hr': '2.152795005'}, {'CO': '0', 'temp': '25', 'hum': '31', 'bat': '96', 'lat': '40.06479192', 'longi': '-8.1604327', 'alt': '1142', 'hr': '1.95093927'}]}";
 
     @Autowired
-    public KafkaTemplate<String, String> template;
-
-    @Autowired
-    private KafkaConsumerTest consumer;
-
-    @Autowired
-    private KafkaProducerTest producer;
-
-    @Value("ESP33_SensorData")
-    private String topic;
-    @Value("ESP33_Alerts")
-    private String topic2;
-
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+    
 
     @Test
-    public void givenEmbeddedKafkaBroker_whenSendingtoSimpleProducer_thenMessageReceived() throws Exception {
-        producer.send(topic, "{'firefighters': [{'CO': '36', 'temp': '23', 'hum': '38', 'bat': '0', 'lat': '40.06483992', 'longi': '-8.16039721', 'alt': '1132', 'hr': '101.525187124'}, {'CO': '0', 'temp': '23', 'hum': '39', 'bat': '93', 'lat': '40.06469093', 'longi': '-8.16050738', 'alt': '1139', 'hr': '2.152795005'}, {'CO': '0', 'temp': '25', 'hum': '31', 'bat': '96', 'lat': '40.06479192', 'longi': '-8.1604327', 'alt': '1142', 'hr': '1.95093927'}]}");
-        //consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    public void testReceivingKafkaEvents() {
         
-        //assertThat(consumer.getLatch().getCount(), equalTo(0L));
-        assertThat(consumer.getPayload(), containsString("FF #1 : Heart Rate is 101.525187124"));
+        Producer<Integer, String> producer = configureProducer();
+
+        producer.send(new ProducerRecord<>(TEST_TOPIC, 123, testMsg));
+
+        Consumer<Integer, String> consumer = configureConsumer();
+
+        ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, TEST_TOPIC);
+        Assertions.assertNotNull(singleRecord);
+        Assertions.assertEquals(singleRecord.key(), 123);
+        System.out.println("VALOR_: "+singleRecord.value());
+        Assertions.assertTrue(singleRecord.value().contains("'hr': '101.525187124'")); //("FF #1 : Heart Rate is 101.525187124"));
+        consumer.close();
+        producer.close();
     }
 
-}*/
+    private Consumer<Integer, String> configureConsumer() {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("FireFighter_2", "true", embeddedKafkaBroker);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        Consumer<Integer, String> consumer = new DefaultKafkaConsumerFactory<Integer, String>(consumerProps)
+                .createConsumer();
+        consumer.subscribe(Collections.singleton(TEST_TOPIC));
+        return consumer;
+    }
+
+    private Producer<Integer, String> configureProducer() {
+        Map<String, Object> producerProps = new HashMap<>(KafkaTestUtils.producerProps(embeddedKafkaBroker));
+        return new DefaultKafkaProducerFactory<Integer, String>(producerProps).createProducer();
+    }
+}
